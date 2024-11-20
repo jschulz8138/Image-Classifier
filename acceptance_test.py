2b2cb2b35c8f8821755ddc55d4775323c3422a6f
stank3391 <115744916+stank3391@users.noreply.github.com>
2024-11-20 18:52:55 -0500

    GREEN IN A SEA OF RED

--- acceptance_test.py
+++ acceptance_test.py
@@ -2,6 +2,7 @@
 import pytest
 import cv2
 from result import result
+from app import app
 
 
 IMAGE_TEST = "static/test_img/plane0.png"
@@ -16,13 +17,23 @@ IMAGE_TEST = "static/test_img/plane0.png"
 
 def test_upload_and_classify_images():
     """This module handles the upload acceptance testing."""
-    actual = result(IMAGE_TEST)
+    client = app.test_client()
+    client.testing = True
 
-    class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
-    assert actual in class_names, f"Unexpected classification result: {actual}"
+    # Fetch the CSRF token
+    response = client.get("/")
+    csrf_token = response.data.decode().split('name="csrf_token" value="')[1].split('"')[0]
+
+    #Rest to save an image
+    with open(IMAGE_TEST, "rb") as img_file:
+        data = {"file": (img_file, IMAGE_TEST),"csrf_token": csrf_token}
+    response = client.post("/", data=data, content_type="multipart/form-data")
+
+    assert response.status_code == 200
+    assert b"Result:" in response.data  # Verifying "Result:" is in the HTML response
 
 
-#Gerkin Acceptance Test One
+#Gerkin Acceptance Test Two
 #
 #Unsupported File Format
 #
@@ -35,6 +46,20 @@ def test_unsupported_file_format():
     """This module handles the testing of file formats"""
     unsupported_file_path = "test_images/document.txt"
 
+    client = app.test_client()
+    client.testing = True
+
+    # Fetch the CSRF token
+    response = client.get("/")
+    csrf_token = response.data.decode().split('name="csrf_token" value="')[1].split('"')[0]
+
+    #Rest to save an image
+    with open(IMAGE_TEST, "rb") as img_file:
+        data = {"file": (img_file, IMAGE_TEST),"csrf_token": csrf_token}
+    response = client.post("/", data=data, content_type="multipart/form-data")
+
     # Call the classify function and expect an error
     with pytest.raises(cv2.error, match=r".*resize.cpp.*Assertion failed.*ssize.empty\(\).*"): # pylint: disable=no-member
         result(unsupported_file_path)
+
+    assert isinstance(cv2.error, Exception) # pylint: disable=no-member
